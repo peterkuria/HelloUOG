245a2dd912921cc7886705afff00c74bfe07f14f
Peter Kuria <peter.kuria.cloud@gmail.com>
2022-08-11 16:54:22 +0300

    initial commit

---
+++ app/src/main/java/com/softavnetworks/hellouog/NameEntry.java
@@ -0,0 +1,14 @@
+package com.softavnetworks.hellouog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NameEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
